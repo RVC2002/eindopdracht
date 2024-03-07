66920713279d74f0b9eeead0b8e1df2d7dc32b34
RickvanCampen <rickvancampen11@gmail.com>
2024-03-07 14:35:33 +0100

    Logo in react toegevoegd zodat deze verschijnt wanneer er op 'run' wordt geklikt.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import WeatherComponent from './components/WeatherComponent';
-
+import logo from './logo.svg';
+import './App.css';
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 {/* Voeg de WeatherComponent toe */}
                 <WeatherComponent />
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
             </header>
         </div>
     );
 }
 
 export default App;
+
