42cf732c7f3ff444e78c64426db22426f86400ee
RickvanCampen <rickvancampen11@gmail.com>
2024-03-13 15:45:48 +0100

    html en css bestanden aangemaakt en gelinkt aan elkaar

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -37,7 +37,7 @@ const WeatherComponent = () => {
                         </option>
                     ))}
                 </select>
-            </label>
+            </label>l
             {weatherData && (
                 <div>
                     <h2>Huidige weer in {weatherData.name}</h2>
