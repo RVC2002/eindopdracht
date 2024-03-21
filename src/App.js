8ecfe0c440301807b17f4f145ae774277081b008
RickvanCampen <rickvancampen11@gmail.com>
2024-03-21 12:04:08 +0100

    navbar.js aangemaakt en een begin gemaakt aan routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WeatherComponent from './components/WeatherComponent';
 import HolidayComponent from './components/HolidayComponent';
 import LoginComponent from './components/LoginComponent';
 import { AuthProvider } from './gebruikersauthenticatiegegevens';
+import Navbar from './components/Navbar'
 
 function App() {
     return (
@@ -11,11 +12,12 @@ function App() {
             <Router>
                 <div className="App">
                     <header className="App-header">
-                        <Switch>
-                            <Route path="/weather" component={WeatherComponent} />
-                            <Route path="/holidays" component={HolidayComponent} />
-                            <Route path="/login" component={LoginComponent} />
-                        </Switch>
+                        <Navbar /> {/* Voeg de Navbar component toe */}
+                        <Routes>
+                            <Route path="/weather" element={<WeatherComponent />} />
+                            <Route path="/holidays" element={<HolidayComponent />} />
+                            <Route path="/login" element={<LoginComponent />} />
+                        </Routes>
                     </header>
                 </div>
             </Router>
